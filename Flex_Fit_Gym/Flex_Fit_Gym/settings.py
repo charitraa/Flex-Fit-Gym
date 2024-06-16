e6c1058ec4f62bf8b331954dbcc580940626aab2
charitraa <stharabi9862187405@gmail.com>
2024-06-16 17:42:59 +0545

    new

--- Flex_Fit_Gym/Flex_Fit_Gym/settings.py
+++ Flex_Fit_Gym/Flex_Fit_Gym/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Gym_App'
+    'Gym_App',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
